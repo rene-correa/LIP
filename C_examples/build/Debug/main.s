392b3aebce9efa9bd893f056ea9258c3e59801d2
Rene Corea <ratanky@hotmail.com>
2024-09-21 14:04:55 -0300

    Folder refactor

---
+++ C_examples/build/Debug/main.s
@@ -0,0 +1,815 @@
+	.file	"main.c"
+	.text
+Ltext0:
+Letext0:
+	.section	.debug_info,"dr"
+Ldebug_info0:
+	.long	0x77
+	.word	0x4
+	.secrel32	Ldebug_abbrev0
+	.byte	0x4
+	.uleb128 0x1
+	.ascii "GNU C11 6.3.0 -mtune=generic -march=i586 -g3 -O0\0"
+	.byte	0xc
+	.ascii "main.c\0"
+	.ascii "G:\\My Drive\\Aula\\_Pos_Sistemas_Embarcados\\LIP\0"
+	.secrel32	Ldebug_line0
+	.secrel32	Ldebug_macro0
+	.section	.debug_abbrev,"dr"
+Ldebug_abbrev0:
+	.uleb128 0x1
+	.uleb128 0x11
+	.byte	0
+	.uleb128 0x25
+	.uleb128 0x8
+	.uleb128 0x13
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0x8
+	.uleb128 0x1b
+	.uleb128 0x8
+	.uleb128 0x10
+	.uleb128 0x17
+	.uleb128 0x2119
+	.uleb128 0x17
+	.byte	0
+	.byte	0
+	.byte	0
+	.section	.debug_aranges,"dr"
+	.long	0x14
+	.word	0x2
+	.secrel32	Ldebug_info0
+	.byte	0x4
+	.byte	0
+	.word	0
+	.word	0
+	.long	0
+	.long	0
+	.section	.debug_macro,"dr"
+Ldebug_macro0:
+	.word	0x4
+	.byte	0x2
+	.secrel32	Ldebug_line0
+	.byte	0x1
+	.uleb128 0
+	.ascii "__STDC__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__STDC_VERSION__ 201112L\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__STDC_UTF_16__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__STDC_UTF_32__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__STDC_HOSTED__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GNUC__ 6\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GNUC_MINOR__ 3\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GNUC_PATCHLEVEL__ 0\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__VERSION__ \"6.3.0\"\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__ATOMIC_RELAXED 0\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__ATOMIC_SEQ_CST 5\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__ATOMIC_ACQUIRE 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__ATOMIC_RELEASE 3\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__ATOMIC_ACQ_REL 4\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__ATOMIC_CONSUME 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FINITE_MATH_ONLY__ 0\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZEOF_INT__ 4\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZEOF_LONG__ 4\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZEOF_LONG_LONG__ 8\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZEOF_SHORT__ 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZEOF_FLOAT__ 4\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZEOF_DOUBLE__ 8\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZEOF_LONG_DOUBLE__ 12\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZEOF_SIZE_T__ 4\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__CHAR_BIT__ 8\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__BIGGEST_ALIGNMENT__ 16\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__ORDER_LITTLE_ENDIAN__ 1234\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__ORDER_BIG_ENDIAN__ 4321\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__ORDER_PDP_ENDIAN__ 3412\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__BYTE_ORDER__ __ORDER_LITTLE_ENDIAN__\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLOAT_WORD_ORDER__ __ORDER_LITTLE_ENDIAN__\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZEOF_POINTER__ 4\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZE_TYPE__ unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__PTRDIFF_TYPE__ int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__WCHAR_TYPE__ short unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__WINT_TYPE__ short unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INTMAX_TYPE__ long long int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINTMAX_TYPE__ long long unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__CHAR16_TYPE__ short unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__CHAR32_TYPE__ unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIG_ATOMIC_TYPE__ int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT8_TYPE__ signed char\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT16_TYPE__ short int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT32_TYPE__ int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT64_TYPE__ long long int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT8_TYPE__ unsigned char\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT16_TYPE__ short unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT32_TYPE__ unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT64_TYPE__ long long unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_LEAST8_TYPE__ signed char\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_LEAST16_TYPE__ short int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_LEAST32_TYPE__ int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_LEAST64_TYPE__ long long int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_LEAST8_TYPE__ unsigned char\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_LEAST16_TYPE__ short unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_LEAST32_TYPE__ unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_LEAST64_TYPE__ long long unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_FAST8_TYPE__ signed char\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_FAST16_TYPE__ short int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_FAST32_TYPE__ int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_FAST64_TYPE__ long long int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_FAST8_TYPE__ unsigned char\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_FAST16_TYPE__ short unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_FAST32_TYPE__ unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_FAST64_TYPE__ long long unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INTPTR_TYPE__ int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINTPTR_TYPE__ unsigned int\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__has_include(STR) __has_include__(STR)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__has_include_next(STR) __has_include_next__(STR)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GXX_ABI_VERSION 1010\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SCHAR_MAX__ 0x7f\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SHRT_MAX__ 0x7fff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_MAX__ 0x7fffffff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LONG_MAX__ 0x7fffffffL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LONG_LONG_MAX__ 0x7fffffffffffffffLL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__WCHAR_MAX__ 0xffff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__WCHAR_MIN__ 0\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__WINT_MAX__ 0xffff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__WINT_MIN__ 0\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__PTRDIFF_MAX__ 0x7fffffff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZE_MAX__ 0xffffffffU\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INTMAX_MAX__ 0x7fffffffffffffffLL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INTMAX_C(c) c ## LL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINTMAX_MAX__ 0xffffffffffffffffULL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINTMAX_C(c) c ## ULL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIG_ATOMIC_MAX__ 0x7fffffff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIG_ATOMIC_MIN__ (-__SIG_ATOMIC_MAX__ - 1)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT8_MAX__ 0x7f\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT16_MAX__ 0x7fff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT32_MAX__ 0x7fffffff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT64_MAX__ 0x7fffffffffffffffLL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT8_MAX__ 0xff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT16_MAX__ 0xffff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT32_MAX__ 0xffffffffU\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT64_MAX__ 0xffffffffffffffffULL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_LEAST8_MAX__ 0x7f\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT8_C(c) c\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_LEAST16_MAX__ 0x7fff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT16_C(c) c\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_LEAST32_MAX__ 0x7fffffff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT32_C(c) c\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_LEAST64_MAX__ 0x7fffffffffffffffLL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT64_C(c) c ## LL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_LEAST8_MAX__ 0xff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT8_C(c) c\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_LEAST16_MAX__ 0xffff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT16_C(c) c\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_LEAST32_MAX__ 0xffffffffU\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT32_C(c) c ## U\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_LEAST64_MAX__ 0xffffffffffffffffULL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT64_C(c) c ## ULL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_FAST8_MAX__ 0x7f\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_FAST16_MAX__ 0x7fff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_FAST32_MAX__ 0x7fffffff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INT_FAST64_MAX__ 0x7fffffffffffffffLL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_FAST8_MAX__ 0xff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_FAST16_MAX__ 0xffff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_FAST32_MAX__ 0xffffffffU\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINT_FAST64_MAX__ 0xffffffffffffffffULL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__INTPTR_MAX__ 0x7fffffff\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__UINTPTR_MAX__ 0xffffffffU\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_IEC_559 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_IEC_559_COMPLEX 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_EVAL_METHOD__ 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC_EVAL_METHOD__ 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_RADIX__ 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_MANT_DIG__ 24\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_DIG__ 6\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_MIN_EXP__ (-125)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_MIN_10_EXP__ (-37)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_MAX_EXP__ 128\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_MAX_10_EXP__ 38\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_DECIMAL_DIG__ 9\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_MAX__ 3.40282346638528859812e+38F\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_MIN__ 1.17549435082228750797e-38F\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_EPSILON__ 1.19209289550781250000e-7F\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_DENORM_MIN__ 1.40129846432481707092e-45F\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_HAS_DENORM__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_HAS_INFINITY__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__FLT_HAS_QUIET_NAN__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DBL_MANT_DIG__ 53\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DBL_DIG__ 15\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DBL_MIN_EXP__ (-1021)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DBL_MIN_10_EXP__ (-307)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DBL_MAX_EXP__ 1024\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DBL_MAX_10_EXP__ 308\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DBL_DECIMAL_DIG__ 17\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DBL_MAX__ ((double)1.79769313486231570815e+308L)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DBL_MIN__ ((double)2.22507385850720138309e-308L)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DBL_EPSILON__ ((double)2.22044604925031308085e-16L)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DBL_DENORM_MIN__ ((double)4.94065645841246544177e-324L)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DBL_HAS_DENORM__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DBL_HAS_INFINITY__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DBL_HAS_QUIET_NAN__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LDBL_MANT_DIG__ 64\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LDBL_DIG__ 18\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LDBL_MIN_EXP__ (-16381)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LDBL_MIN_10_EXP__ (-4931)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LDBL_MAX_EXP__ 16384\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LDBL_MAX_10_EXP__ 4932\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DECIMAL_DIG__ 21\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LDBL_MAX__ 1.18973149535723176502e+4932L\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LDBL_MIN__ 3.36210314311209350626e-4932L\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LDBL_EPSILON__ 1.08420217248550443401e-19L\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LDBL_DENORM_MIN__ 3.64519953188247460253e-4951L\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LDBL_HAS_DENORM__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LDBL_HAS_INFINITY__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__LDBL_HAS_QUIET_NAN__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC32_MANT_DIG__ 7\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC32_MIN_EXP__ (-94)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC32_MAX_EXP__ 97\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC32_MIN__ 1E-95DF\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC32_MAX__ 9.999999E96DF\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC32_EPSILON__ 1E-6DF\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC32_SUBNORMAL_MIN__ 0.000001E-95DF\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC64_MANT_DIG__ 16\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC64_MIN_EXP__ (-382)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC64_MAX_EXP__ 385\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC64_MIN__ 1E-383DD\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC64_MAX__ 9.999999999999999E384DD\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC64_EPSILON__ 1E-15DD\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC64_SUBNORMAL_MIN__ 0.000000000000001E-383DD\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC128_MANT_DIG__ 34\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC128_MIN_EXP__ (-6142)\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC128_MAX_EXP__ 6145\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC128_MIN__ 1E-6143DL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC128_MAX__ 9.999999999999999999999999999999999E6144DL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC128_EPSILON__ 1E-33DL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DEC128_SUBNORMAL_MIN__ 0.000000000000000000000000000000001E-6143DL\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__REGISTER_PREFIX__ \0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__USER_LABEL_PREFIX__ _\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GNUC_STDC_INLINE__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__NO_INLINE__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_HAVE_SYNC_COMPARE_AND_SWAP_1 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_HAVE_SYNC_COMPARE_AND_SWAP_2 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_HAVE_SYNC_COMPARE_AND_SWAP_8 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_ATOMIC_BOOL_LOCK_FREE 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_ATOMIC_CHAR_LOCK_FREE 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_ATOMIC_CHAR16_T_LOCK_FREE 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_ATOMIC_CHAR32_T_LOCK_FREE 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_ATOMIC_WCHAR_T_LOCK_FREE 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_ATOMIC_SHORT_LOCK_FREE 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_ATOMIC_INT_LOCK_FREE 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_ATOMIC_LONG_LOCK_FREE 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_ATOMIC_LLONG_LOCK_FREE 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_ATOMIC_TEST_AND_SET_TRUEVAL 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_ATOMIC_POINTER_LOCK_FREE 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_HAVE_DWARF2_CFI_ASM 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__PRAGMA_REDEFINE_EXTNAME 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZEOF_WCHAR_T__ 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZEOF_WINT_T__ 2\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZEOF_PTRDIFF_T__ 4\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__i386 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__i386__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "i386 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZEOF_FLOAT80__ 12\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SIZEOF_FLOAT128__ 16\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__ATOMIC_HLE_ACQUIRE 65536\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__ATOMIC_HLE_RELEASE 131072\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GCC_ASM_FLAG_OUTPUTS__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__i586 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__i586__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__pentium 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__pentium__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__code_model_32__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SEG_FS 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__SEG_GS 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "_X86_ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__stdcall __attribute__((__stdcall__))\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__fastcall __attribute__((__fastcall__))\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__thiscall __attribute__((__thiscall__))\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__cdecl __attribute__((__cdecl__))\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "_stdcall __attribute__((__stdcall__))\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "_fastcall __attribute__((__fastcall__))\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "_thiscall __attribute__((__thiscall__))\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "_cdecl __attribute__((__cdecl__))\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GXX_MERGED_TYPEINFO_NAMES 0\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__GXX_TYPEINFO_EQUALITY_INLINE 0\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__MSVCRT__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__MINGW32__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "_WIN32 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__WIN32 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__WIN32__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "WIN32 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__WINNT 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__WINNT__ 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "WINNT 1\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "_INTEGRAL_MAX_BITS 64\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__declspec(x) __attribute__((x))\0"
+	.byte	0x1
+	.uleb128 0
+	.ascii "__DECIMAL_BID_FORMAT__ 1\0"
+	.file 1 "main.c"
+	.byte	0x3
+	.uleb128 0
+	.uleb128 0x1
+	.byte	0x4
+	.byte	0
+	.section	.debug_line,"dr"
+Ldebug_line0:
+	.section	.debug_str,"dr"
+	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
